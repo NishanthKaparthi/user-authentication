7ecf7c76b5622f20bd86b56828093029af02792c
Nishanth Kaparthi <kaparthinishanth328@gmail.com>
2025-07-07 20:32:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# user-authentication
+This repository houses a full-stack user authentication application, connecting a backend API with a frontend interface.
---
+++ src/test/java/com/project/authentication/user_auth_app/UserAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.authentication.user_auth_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
